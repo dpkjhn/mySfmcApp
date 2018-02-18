f9f5195caff21dc5cbf100eec5cbbdcef1d7dff7
Deepak John <deepakjohn@outlook.com>
2018-02-18 21:12:15 +0000

    Remove the now ignored directory node_modules

--- dist/modules/sfmc-endpoints.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=sfmc-endpoints.js.map
--- dist/modules/sfmc-helper.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import fs from 'fs';
-
-module.exports.base64_encode = function (file) {
-    let bitmap = fs.readFileSync(file);
-    return new Buffer(bitmap).toString('base64');
-};
-//# sourceMappingURL=sfmc-helper.js.map
